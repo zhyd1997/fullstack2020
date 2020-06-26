87718479a5101c79dbee4093d6ff7ef8bdaa6068
007tom <zhyd007@gmail.com>
2020-06-26 15:14:02 +0800

    Part2 phonebook step 3

--- part2/README.md
+++ part2/README.md
@@ -1,7 +1,8 @@
 - [x] 2.1 Course contents step6
 - [x] 2.2 Course contents step7
 - [x] 2.3 Course contents step8 *
-- [x] 2.4: Course contents step9
-- [x] 2.5: separate module
-- [x] 2.6: The Phonebook Step1
-- [x] 2.7: The Phonebook Step2
+- [x] 2.4 Course contents step9
+- [x] 2.5 separate module
+- [x] 2.6 The Phonebook Step1
+- [x] 2.7 The Phonebook Step2
+- [x] 2.8 The Phonebook Step3
--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,20 +2,27 @@ import React, {useState} from 'react';
 
 const App = () => {
 	const [persons, setPersons] = useState([
-		{name: 'Arto Hellas'}
+		{name: 'Arto Hellas', text: '040-1234567'}
 	])
 	const [newName, setNewName] = useState('')
+	const [newNumber, setNewNumber] = useState('')
 
 	const addPerson = (event) => {
 		event.preventDefault()
-		const personName = {name: newName}
-		setPersons(persons.concat(personName))
-		alert(`${newName} is already added to phonebook`)
+		const person = {name: newName, text: newNumber}
+		setPersons(persons.concat(person))
+		console.log(persons)
+		alert(`${newName} ${newNumber} are already added to phonebook`)
 		setNewName('')
+		setNewNumber('')
 	}
 
 	const handlePersonChange = (event) => {
 		setNewName(event.target.value)
+	}
+
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value)
 	}
 
 	return (
@@ -23,14 +30,17 @@ const App = () => {
 			<h2>Phonebook</h2>
 			<form onSubmit={addPerson}>
 				<div>
-					name: <input onChange={handlePersonChange} />
+					name: <input value={newName} onChange={handlePersonChange} />
+				</div>
+				<div>
+					number: <input value={newNumber} onChange={handleNumberChange} />
 				</div>
 				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			{persons.map(i => <span key={i.name}>{i.name}<br /></span>)}
+			{persons.map(i => <span key={i.name}>{i.name}&nbsp;{i.text}<br /></span>)}
 		</div>
 	)
 }
